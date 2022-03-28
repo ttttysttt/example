be8a8b2eb146d154b88acaa3d555448203a62ec1
tys1336749461 <tys1336749461@gmail.com>
2022-03-28 16:54:23 +0800

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,3 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("hello world")
